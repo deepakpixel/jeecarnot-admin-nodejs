a0900dae8db33d6742d44546a319789d6121f89f
Lakshay Angrish <lakshay.angrish@hotmail.com>
2020-11-19 17:20:53 +0530

    use async await

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -3,43 +3,38 @@ require("dotenv").config();
 const router = require("express").Router();
 const checkAuth = require("../middleware/checkAuth");
 
-router.get("/msg91-balance", checkAuth, (req, res, next) => {
-  let responses = [];
+router.get("/msg91-balance", checkAuth, async (req, res, next) => {
+  try {
+    let promotional = await axios.get(
+      "https://api.msg91.com/api/balance.php?authkey=" +
+        process.env.msg91AuthKey +
+        "&type=1"
+    );
 
-  axios
-    .all([
-      axios.get(
-        "https://api.msg91.com/api/balance.php?authkey=" +
-          process.env.msg91AuthKey +
-          "&type=1"
-      ),
-      axios.get(
-        "https://api.msg91.com/api/balance.php?authkey=" +
-          process.env.msg91AuthKey +
-          "&type=4"
-      ),
-      axios.get(
-        "https://api.msg91.com/api/balance.php?authkey=" +
-          process.env.msg91AuthKey +
-          "&type=106"
-      ),
-    ])
-    .then(
-      axios.spread((response1, response2, response3) => {
-        res.status(200).json({
-          promotional: response1.data,
-          transactional: response2.data,
-          sendOTP: response3.data,
-        });
-      })
-    )
-    .catch((error) => {
-      console.log(error);
-      res.status(500).json({
-        type: "error",
-        message: error.message,
-      });
+    let transactional = await axios.get(
+      "https://api.msg91.com/api/balance.php?authkey=" +
+        process.env.msg91AuthKey +
+        "&type=4"
+    );
+
+    let sendOTP = await axios.get(
+      "https://api.msg91.com/api/balance.php?authkey=" +
+        process.env.msg91AuthKey +
+        "&type=106"
+    );
+
+    res.status(200).json({
+      promotional: promotional.data,
+      transactional: transactional.data,
+      sendOTP: sendOTP.data,
     });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      type: "error",
+      message: error.message,
+    });
+  }
 });
 
 module.exports = router;
