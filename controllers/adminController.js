7cc7eccb3561b12ae61728f8044ca32541a4818b
Lakshay Angrish <lakshay.angrish@hotmail.com>
2020-11-20 17:41:40 +0530

    restructuring

---
+++ controllers/adminController.js
@@ -0,0 +1,169 @@
+const axios = require("axios");
+const Mentee = require("../models/mentee");
+const Mentor = require("../models/mentor");
+
+exports.getMSG91Balance = async (req, res, next) => {
+  try {
+    let responses = await Promise.all([
+      axios.get(
+        "https://api.msg91.com/api/balance.php?authkey=" +
+          process.env.msg91AuthKey +
+          "&type=1"
+      ),
+      axios.get(
+        "https://api.msg91.com/api/balance.php?authkey=" +
+          process.env.msg91AuthKey +
+          "&type=4"
+      ),
+      axios.get(
+        "https://api.msg91.com/api/balance.php?authkey=" +
+          process.env.msg91AuthKey +
+          "&type=106"
+      ),
+    ]);
+
+    res.status(200).json({
+      promotional: responses[0].data,
+      transactional: responses[1].data,
+      sendOTP: responses[2].data,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      type: "error",
+      message: error.message,
+    });
+  }
+};
+
+function escapeRegex(text) {
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+}
+
+exports.menteeSearch = async (req, res, next) => {
+  try {
+    if (
+      !req.body.searchType ||
+      !req.body.searchQuery ||
+      !req.body.page ||
+      !req.body.perPage
+    ) {
+      throw new Error(
+        "searchType, searchQuery, page & perPage must be supplied"
+      );
+    }
+
+    if (
+      typeof req.body.page != "number" ||
+      typeof req.body.perPage != "number"
+    ) {
+      throw new Error("page and perPage must be numbers");
+    }
+
+    let searchFields = [];
+    const regex = new RegExp(escapeRegex(req.body.searchQuery), "gi");
+
+    if (req.body.searchType == "name") {
+      searchFields.push({ name: regex });
+    } else if (req.body.searchType == "phone") {
+      searchFields.push({ phone: regex });
+    } else if (req.body.searchType == "email") {
+      searchFields.push({ email: regex });
+    } else {
+      throw new Error("invalid searchType");
+    }
+
+    let responses = await Promise.all([
+      Mentee.find({
+        $or: searchFields,
+      })
+        .countDocuments()
+        .exec(),
+      Mentee.find({
+        $or: searchFields,
+      })
+        .skip((req.body.page - 1) * req.body.perPage)
+        .limit(req.body.perPage)
+        .exec(),
+    ]);
+
+    let totalResults = responses[0];
+    let results = responses[1];
+
+    console.log(results);
+    return res.status(200).json({
+      totalResults,
+      results,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      type: "error",
+      message: error.message,
+    });
+  }
+};
+
+exports.mentorSearch = async (req, res, next) => {
+  try {
+    if (
+      !req.body.searchType ||
+      !req.body.searchQuery ||
+      !req.body.page ||
+      !req.body.perPage
+    ) {
+      throw new Error(
+        "searchType, searchQuery, page & perPage must be supplied"
+      );
+    }
+
+    if (
+      typeof req.body.page != "number" ||
+      typeof req.body.perPage != "number"
+    ) {
+      throw new Error("page and perPage must be numbers");
+    }
+
+    let searchFields = [];
+    const regex = new RegExp(escapeRegex(req.body.searchQuery), "gi");
+
+    if (req.body.searchType == "name") {
+      searchFields.push({ name: regex });
+    } else if (req.body.searchType == "phone") {
+      searchFields.push({ phone: regex });
+    } else if (req.body.searchType == "email") {
+      searchFields.push({ email: regex });
+    } else {
+      throw new Error("invalid searchType");
+    }
+
+    let responses = await Promise.all([
+      Mentor.find({
+        $or: searchFields,
+      })
+        .countDocuments()
+        .exec(),
+      Mentor.find({
+        $or: searchFields,
+      })
+        .skip((req.body.page - 1) * req.body.perPage)
+        .limit(req.body.perPage)
+        .exec(),
+    ]);
+
+    let totalResults = responses[0];
+    let results = responses[1];
+
+    console.log(results);
+    return res.status(200).json({
+      totalResults,
+      results,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      type: "error",
+      message: error.message,
+    });
+  }
+};
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,9 +1,7 @@
 const router = require("express").Router();
 const passport = require("passport");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
-const User = require("../models/admin");
 const checkAuth = require("../middleware/checkAuth");
+const authController = require("../controllers/authController");
 
 router.get("/login", (req, res) => {
   if (req.isAuthenticated()) {
@@ -32,38 +30,7 @@ router.get("/change-password", checkAuth, (req, res) => {
   res.render("changePassword");
 });
 
-router.post("/change-password", checkAuth, async (req, res) => {
-  try {
-    if (req.body.newPassword == "") {
-      throw new Error("Password field cannot be empty");
-    }
-    if (!req.user) {
-      throw new Error("User data unavailable");
-    }
-    let result = await bcrypt.compare(req.body.oldPassword, req.user.password);
-    if (!result) {
-      throw new Error("old password incorrect");
-    }
-    console.log("new password");
-
-    let hashedPassword = await bcrypt.hash(req.body.newPassword, 10);
-    await User.findByIdAndUpdate(req.user._id, {
-      password: hashedPassword,
-    }).exec();
-
-    req.logout();
-    res.status(200).json({
-      type: "success",
-      message:
-        "Password changed. Login again at http://localhost:3000/auth/login",
-    });
-  } catch (error) {
-    res.status(500).json({
-      type: "error",
-      error: error.message,
-    });
-  }
-});
+router.post("/change-password", checkAuth, authController.changePassword);
 
 router.get("/password", (req, res) => {
   if (!req.isAuthenticated()) {
@@ -72,47 +39,6 @@ router.get("/password", (req, res) => {
   res.render("password");
 });
 
-router.post("/password", async (req, res) => {
-  try {
-    if (!req.isAuthenticated()) {
-      throw new Error("User not authenticated");
-    }
-
-    let result = await bcrypt.compare(req.body.password, req.user.password);
-    let token;
-    if (result === true) {
-      console.log("passwords matched");
-      try {
-        token = req.cookies.token;
-        jwt.verify(token, process.env.jwtKey);
-        console.log("token valid");
-      } catch (error) {
-        console.log("new token");
-        token = jwt.sign(
-          {
-            id: req.user._id,
-          },
-          process.env.jwtKey,
-          {
-            expiresIn: "1h",
-          }
-        );
-      }
-
-      res.cookie("token", token);
-      return res.status(200).json({
-        type: "success",
-        message: "password verified",
-      });
-    } else {
-      throw new Error("incorrect password");
-    }
-  } catch (error) {
-    res.status(500).json({
-      type: "error",
-      error: error.message,
-    });
-  }
-});
+router.post("/password", authController.verifyPassword);
 
 module.exports = router;
