c563797d1a653ddec253ccd57688ce73965be8ba
Lakshay Angrish <lakshay.angrish@hotmail.com>
2020-11-21 10:36:08 +0530

    aadd feedback and request models

---
+++ models/request.js
@@ -0,0 +1,18 @@
+var mongoose = require("mongoose");
+
+var requestSchema = mongoose.Schema(
+  {
+    menteeID: {
+      type: String,
+      default: "",
+    },
+    material: String,
+    status: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Request", requestSchema);
